ae9fbda7fb49eca6fd1a71f9f3a2382b28ed3b51
ajinkyadandvate <46273685+ajinkyadandvate@users.noreply.github.com>
2021-10-16 17:24:11 +0530

    basemodel added

--- Sms.py
+++ Sms.py
@@ -1,7 +1,29 @@
 from functools import total_ordering
 
+class BaseModel(pydantic.BaseModel):
+    """All data models inherit from this class"""
 
-class Sms(object):
+    @pydantic.root_validator(pre=True)
+    def _min_properties(cls, data):
+        """At least one property is required"""
+        if not data:
+            raise ValueError("At least one property is required")
+        return data
+
+    def dict(self, include_nulls=False, **kwargs):
+        """Override the super dict method by removing null keys from the dict, unless include_nulls=True"""
+        kwargs["exclude_none"] = not include_nulls
+        return super().dict(**kwargs)
+
+    class Config:
+        extra = pydantic.Extra.forbid  # forbid sending additional fields/properties
+        anystr_strip_whitespace = True  # strip whitespaces from strings
+
+class Sms(BaseModel):
+
+    msg : str
+    date : str
+
     def __init__(self, msg, date):
         self.msg = msg
         self.date = date
@@ -9,4 +31,4 @@ class Sms(object):
     
     def get_month(self):
         split_date =  self.date.split('-')
-        return split_date[0] + split_date[1]
+        return split_date[0] + '-' +split_date[1]
